8ff8b2ff27d98c0a9b7b4defe33fdf6bcb842287
anand1827 <bhusharajuanand2002@gmail.com>
2025-03-03 11:55:34 +0530

    Added a new feature

---
+++ sum.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter the number1"))
+number2 = int(input("Enter the number2"))
+sum = number1 + number2
+diff =number1 - number2
+addition = 'The sum of {number1} and {number2} is {sum}'
+subtraction = 'The subtraction of {number1} and {number2} is {diffgit}'
