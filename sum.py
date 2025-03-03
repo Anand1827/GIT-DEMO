a5a30e52f4f0c44f524da3cb1d3646ec2b65902b
anand1827 <bhusharajuanand2002@gmail.com>
2025-03-03 16:34:32 +0530

    changed num1

--- sum.py
+++ sum.py
@@ -1,8 +1,8 @@
-number1 = int(input("Enter the number1"))
-number2 = int(input("Enter the number2"))
-sum = number1 + number2
-diff =number1 - number2
-div = number1 / number2
-print('The sum of {number1} and {number2} is {sum}')
-print('The subtraction of {number1} and {number2} is {diff}')
-print('The division of {number1} and {number2} is {div}')
+num1 = int(input("Enter the number1"))
+num2 = int(input("Enter the number2"))
+sum = num1 + number2
+diff =num1 - number2
+div = num1 / number2
+print('The sum of {num1} and {number2} is {sum}')
+print('The subtraction of {num1} and {number2} is {diff}')
+print('The division of {num1} and {number2} is {div}')
