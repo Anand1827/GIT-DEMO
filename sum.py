c01b83bccbb03ee5c5e87251d166d1b21090935a
anand1827 <bhusharajuanand2002@gmail.com>
2025-03-03 15:03:22 +0530

    added division

--- sum.py
+++ sum.py
@@ -2,5 +2,7 @@ number1 = int(input("Enter the number1"))
 number2 = int(input("Enter the number2"))
 sum = number1 + number2
 diff =number1 - number2
-addition = 'The sum of {number1} and {number2} is {sum}'
-subtraction = 'The subtraction of {number1} and {number2} is {diffgit}'
+div = number1 / number2
+print('The sum of {number1} and {number2} is {sum}')
+print('The subtraction of {number1} and {number2} is {diff}')
+print('The division of {number1} and {number2} is {div}')
